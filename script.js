6a9c084e953cf076950f2ce012ec409b8dab02a4
jfossi518 <jfossi@unbosque.edu.co>
2026-04-10 08:41:01 -0500

    Add horasExtra and calculate totalDevengado

--- script.js
+++ script.js
@@ -4,8 +4,12 @@ let tipoDocumento = ""
 let numeroDocumento = ""
 let salario = 0; 
 let comisiones = 0; 
+let horasExtra = 0; 
 const ClasificaciónDeRiesgo1 = "Mínimo" 
 const ClasificaciónDeRiesgo2 = "Bajo"
 const ClasificaciónDeRiesgo3 = "Medio"
 const ClasificaciónDeRiesgo4 = "Alto"
 const ClasificaciónDeRiesgo5 = "Maximo"
+
+//Operaciones//
+let totalDevengado = salario + comisiones + horasExtra;
